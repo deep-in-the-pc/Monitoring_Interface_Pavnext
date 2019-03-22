3a60bdc5a58035efcf027923fd0a184aed22902d
deep_in_the_pc <1160640@isep.ipp.pt>
2019-03-22 11:40:13 +0000

    Initial Commit

---
+++ GUI/MIP_GUI.py
@@ -0,0 +1,156 @@
+#for serial comms
+import serial.tools.list_ports
+import serial
+import sys
+from time import sleep
+from util import *
+
+#for UI
+from PyQt5 import QtWidgets, QtGui, QtCore
+from gui.MI_GUI_01 import Ui_MainWindow
+
+#for plots
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import random
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        #initialize combo box
+        self.getCOMList()
+
+        if self.ui.targetComCB.currentText()=='':
+            self.serialCOM = None
+        else:
+            self.serialCOM = self.ui.targetComCB.currentText()
+
+        #window arrangement
+        self.currentSetup = None
+
+        #configure serial connection
+        self.serialConnection = serial.Serial()
+        self.serialConnection.bytesize = serial.EIGHTBITS
+        self.serialConnection.parity = serial.PARITY_NONE
+        self.serialConnection.stopbits = serial.STOPBITS_ONE
+        self.serialConnection.timeout = 0.5
+        self.serialConnection.baudrate = 500000
+
+        #button Callbacks
+        self.ui.targetComConnectButton.clicked.connect(self.targetConnectionCB)
+        #combo box Callback
+        self.ui.targetComCB.activated[str].connect(self.onTargetComCBActivated)
+
+    #COMMUNICATION
+
+    def getCOMList(self):
+        self.ui.targetComCB.addItems([comport.device for comport in serial.tools.list_ports.comports()])
+
+    def targetConnectionCB(self):
+
+        if(self.serialConnection.is_open):
+            self.closeConnetion()
+            self.ui.targetComConnectButton.setText("connect")
+        elif(self.serialCOM != None):
+            if(self.establishConnection()):
+                sleep(2) #Wait 1s between connection and first contact (atleast 2 is necessary for arduino to get ready)
+                self.currentSetup = self.queryType()
+                if(self.currentSetup == 0):
+                    QtWidgets.QMessageBox(self, "Warning", "Connected but could not receive response from board")
+                    self.currentSetup = None
+                    self.closeConnetion()
+                else:
+                    # If connection is established set text as disconnect
+                    self.ui.targetComConnectButton.setText("disconnect")
+                    self.addGraphs(self.currentSetup)
+
+    def onTargetComCBActivated(self, text):
+        if text != None:
+            self.serialCOM = text
+
+    def establishConnection(self):
+        try:
+            self.serialConnection.port = self.serialCOM
+            self.serialConnection.open()
+            if(self.serialConnection.is_open):
+                self.ui.connectionStatusLabel.setText("Connection Status: Online")
+                return 1
+            else:
+                return 0
+        except Exception as err:
+            print(str(err))
+    def closeConnetion(self):
+        self.serialConnection.close()
+        self.ui.connectionStatusLabel.setText("Connection Status: Offline")
+
+    def queryType(self):
+        try:
+            #send type request
+            self.serialConnection.write(stringToAscii('1\n'))            #receive type request
+            text_received = self.serialConnection.readline()
+            if(text_received == b''):
+                return 0
+            else:
+                text_received = text_received.decode('ascii')
+                return text_received[:-2] #removes /r/n from end of string
+        except Exception as err:
+            print(str(err))
+
+    #ALTERING GUI
+
+    def addGraphs(self, string):
+        L = 0 #potenciometro linear
+        E = 0 #encoder linear
+        T = 0 # ToF
+        A = 0 # acelarometro
+        E = 0 # extensometro
+        H = 0 # Hall
+        for vars in string.split('-'):
+
+        try:
+            grid = QtWidgets.QGridLayout()
+            self.ui.graphFrame.setLayout(grid)
+            graph1 = PlotCanvas()
+            grid.addWidget(graph1, 0,0)
+            self.move(300, 150)
+            self.setWindowTitle('Calculator')
+            self.show()
+        except Exception as err:
+            print(str(err))
+class PlotCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ GUI/gui/MI_GUI_01.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'monitoring_interface_01.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.setEnabled(True)
+        MainWindow.resize(800, 600)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setAnimated(True)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.connectionFrame = QtWidgets.QFrame(self.centralwidget)
+        self.connectionFrame.setGeometry(QtCore.QRect(20, 20, 191, 91))
+        self.connectionFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.connectionFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.connectionFrame.setObjectName("connectionFrame")
+        self.formLayout_2 = QtWidgets.QFormLayout(self.connectionFrame)
+        self.formLayout_2.setObjectName("formLayout_2")
+        self.targetComLabel = QtWidgets.QLabel(self.connectionFrame)
+        self.targetComLabel.setObjectName("targetComLabel")
+        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.targetComLabel)
+        self.targetComConnectButton = QtWidgets.QPushButton(self.connectionFrame)
+        self.targetComConnectButton.setObjectName("targetComConnectButton")
+        self.formLayout_2.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.targetComConnectButton)
+        self.targetComCB = QtWidgets.QComboBox(self.connectionFrame)
+        self.targetComCB.setObjectName("targetComCB")
+        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.targetComCB)
+        self.connectionStatusLabel = QtWidgets.QLabel(self.connectionFrame)
+        self.connectionStatusLabel.setObjectName("connectionStatusLabel")
+        self.formLayout_2.setWidget(1, QtWidgets.QFormLayout.SpanningRole, self.connectionStatusLabel)
+        self.toolsFrame = QtWidgets.QFrame(self.centralwidget)
+        self.toolsFrame.setGeometry(QtCore.QRect(20, 130, 191, 431))
+        self.toolsFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.toolsFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.toolsFrame.setObjectName("toolsFrame")
+        self.graphFrame = QtWidgets.QFrame(self.centralwidget)
+        self.graphFrame.setGeometry(QtCore.QRect(230, 20, 541, 541))
+        self.graphFrame.setObjectName("graphFrame")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Monitoring Interface V0.1 - Pavnext"))
+        self.targetComLabel.setText(_translate("MainWindow", "Target COM:"))
+        self.targetComConnectButton.setText(_translate("MainWindow", "Connect"))
+        self.connectionStatusLabel.setText(_translate("MainWindow", "Connection Status: Offline"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
