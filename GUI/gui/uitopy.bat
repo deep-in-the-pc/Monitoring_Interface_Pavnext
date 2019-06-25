0c939c077cd996a84bf6627f6de8b21e09f02045
David Carvalho <1160640@isep.ipp.pt>
2019-06-25 23:08:23 +0100

    Initial Commit

--- GUI/gui/uitopy.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\deman\PycharmProjects\Monitoring_Interface_Pavnext\venv\Scripts\python.exe -m PyQt5.uic.pyuic -x monitoring_interface_0301.ui -o MI_GUI_0301.py
--- GUI/util.py
+++
@@ -1,8 +0,0 @@
-def charToAscii(char):
-    return str(chr(ord(char))).encode('ascii')
-
-def stringToAscii(string):
-    returnString = str()
-    for i in string:
-        returnString = returnString + charToAscii(i).decode('ascii')
-    return returnString.encode('ascii')
