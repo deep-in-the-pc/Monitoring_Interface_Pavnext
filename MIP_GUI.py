bd0fe8dbc98d95e374c1318b18817aa29d2980c5
David Carvalho <1160640@isep.ipp.pt>
2019-07-08 21:32:12 +0100

    Added temporary data processing to received raw data

--- MIP_GUI.py
+++ MIP_GUI.py
@@ -245,7 +245,23 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         for idx, inst in enumerate(time):
 
             self.graphsContainer[unit][type]['sensors'][sensor]['time'][inst] = inst
-            self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = rawData[idx]
+            lookup = {0: "Deslocamento linear vertical", 1: "Deslocamento linear horizontal", 2: "Acelerómetro  Eixo X",
+                      3: "Acelerómetro Eixo Y", 4: "Acelerómetro Eixo Z", 5: "Extensômetro", 6: "Encoder Linear",
+                      7: "temperatura", 8: "humidade", 9: "luminosidade", 10: "tensão", 11: "corrente",
+                      12: "rotações pulsos", 13: "rotações hall", 14: "time of flight"}
+
+            if type == "Deslocamento linear vertical" or type == "Deslocamento linear horizontal":
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = (rawData[idx]/1024.0)*30.0
+            elif type == "Acelerómetro  Eixo X" or type == "Acelerómetro Eixo Y" or type == "Acelerómetro Eixo Z":
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = (((rawData[idx] / 1024.0) * 3.3)-1.65)/0.0065
+            elif type == "tensão":
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = (rawData[idx] / 1024.0) * 33.0
+            elif type == "corrente":
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = (rawData[idx] / 1024.0) * 3.3
+            elif type == "rotações pulsos" or "rotações hall":
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = rawData[idx]
+            else:
+                self.graphsContainer[unit][type]['sensors'][sensor]['data'][inst] = rawData[idx]
 
         self.graphsContainer[unit][type]['sensors'][sensor]['pos'] = inst
 
